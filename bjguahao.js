abfb8363ffdf02eb8282bc30b9c6e3d8da8a47fd
luoyingwen <luoyingwen@gmail.com>
2012-12-03 08:39:14 +0800

    handle query page

--- bjguahao.js
+++ bjguahao.js
@@ -1,8 +1,6 @@
-﻿//this function for submit page
-//http://www.bjguahao.gov.cn/comm/beiyi3/guahao.php?hpid=142&ksid=1110101&datid=83033&wday=&sx=1&eday=2012-12-04
+﻿//http://www.bjguahao.gov.cn/comm/beiyi3/guahao.php?hpid=142&ksid=1110101&datid=83033&wday=&sx=1&eday=2012-12-04
 (function(){
 	var fullURL = document.URL.toString();
-	console.log(fullURL);
 	var isGuaHao = (fullURL.search("guahao.php") > 0);
 	if (isGuaHao){
 		console.log("In guahao.php page");
@@ -43,29 +41,26 @@
 
 //http://www.bjguahao.gov.cn/comm/beiyi3/ksyy.php?ksid=1110101&hpid=142&wday=&sx=1&eday=2012-12-04
 (function(){
-	console.log("new function3");
 	var fullURL = document.URL.toString();
-	console.log(fullURL);
 	var isksyy = (fullURL.search("ksyy.php") > 0);
 	if (isksyy){
 		console.log("In ksyy.php");
 		const TIMER_INTERVAL_MS = 100;
 		var timeridBook = setInterval(function(){
-			var anchorArray = document.getElementsByTagName("tr");
+			var anchorArray = document.getElementsByTagName("a");
 			console.log(anchorArray.length);
 			var i = 0;
 			for (i = 0; i < anchorArray.length; i++){
 				var oneAnchor = anchorArray[i];
-				console.log(oneAnchor.text);
-				if (oneAnchor.text === "预约挂号"){
+				var trInnerText = oneAnchor.parentNode.parentNode.innerText.toString();
+				if (oneAnchor.text === "预约挂号" && trInnerText.search("颈椎") != -1 && trInnerText.search("14.00") != -1){
 					clearInterval(timeridBook);
-					console.log(oneAnchor.href);
-					console.log(oneAnchor.parentNode.parentNode.text);
+					console.log(trInnerText);
 					//oneAnchor.click();
 					break;
 				}
 			}
-			if ( i === anchorArray.length ){
+			if ( i > 0 && i === anchorArray.length ){				
 				document.location.reload(true);
 			}		
 		},
